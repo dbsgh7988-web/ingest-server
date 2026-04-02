dc6c73d789901b6be02d851413f59246d98f8505
dbsgh7988-web <dbsgh7988@gmail.com>
2026-04-02 18:53:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/ingest", methods=["POST"])
+@app.route("/ingest", methods=["GET", "POST"])
 def ingest():
-    data = request.json
-    text = data.get("text", "")
+    text = ""
+
+    # 1️⃣ JSON
+    if request.is_json:
+        data = request.get_json()
+        text = data.get("text", "")
+
+    # 2️⃣ form 방식
+    elif request.form:
+        text = request.form.get("text", "")
+
+    # 3️⃣ GET 방식
+    elif request.args:
+        text = request.args.get("text", "")
+
+    # 4️⃣ raw
+    else:
+        text = request.data.decode("utf-8")
 
     print("📩 수신:", text)
 
